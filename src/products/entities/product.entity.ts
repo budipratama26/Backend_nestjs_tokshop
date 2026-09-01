a2df07b62b02a9ef977f2615583f1f2053e02adf
Satria <satriabudi3606@gmail.com>
2026-09-01 15:59:04 +0700

    feat: create Product entity with id, name, price, quantity, and description fields

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
 export class Product {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     name: string;
+    @Column()
     price: number;
+    @Column()
     quantity: number;
+    @Column()
     description: string;
 }
