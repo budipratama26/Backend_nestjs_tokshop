934f762bc1097fa94feff39e9e4ea5e529d938ed
Satria <satriabudi3606@gmail.com>
2026-09-09 11:07:09 +0700

    feat: implement ACID database transaction in orders service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto.js';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { Order } from './entities/order.entity.js';
 import { Product } from '../products/entities/product.entity.js';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,39 +13,45 @@ export class OrdersService {
 
     @InjectRepository(Product)
     private productRepository: Repository<Product>,
+
+    private dataSource: DataSource,
   ) { }
 
   async create(CreateOrderDto: CreateOrderDto, user: any) {
-    const product = await this.productRepository.findOneBy({ id: CreateOrderDto.productId });
-    if (!product) {
-      throw new NotFoundException(`Produk dengan ID ${CreateOrderDto.productId} tidak ditemukan!`);
-    }
-    if (product.quantity < CreateOrderDto.quantity) {
-      throw new BadRequestException(`Stok tidak mencukupi! Sisa stok saat ini hanya: ${product.quantity}`);
-    }
-    product.quantity -= CreateOrderDto.quantity;
-    await this.productRepository.save(product);
-
-    const totalPrice = product.price * CreateOrderDto.quantity;
-    const newOrder = this.orderRepository.create({
-      quantity:
-        CreateOrderDto.quantity,
-      totalPrice: totalPrice,
-      user: { id: user.sub },
-      product: { id: product.id },
+    return await this.dataSource.transaction(async (manager) => {
+      const product = await manager.findOne(Product, {
+        where: {
+          id:
+            CreateOrderDto.productId
+        },
+      });
+      if (!product) {
+        throw new NotFoundException(`Produk dengan ID ${CreateOrderDto.productId} tidak ditemukan!`);
+      }
+      if (product.quantity < CreateOrderDto.quantity) {
+        throw new BadRequestException(`Stok tidak mencukupi! Sisa stok saat ini ${product.quantity}`);
+      }
+      product.quantity -= CreateOrderDto.quantity;
+      await manager.save(Product, product);
+      const totalPrice = product.price * CreateOrderDto.quantity;
+      const newOrder = manager.create(Order, {
+        quantity: CreateOrderDto.quantity,
+        totalPrice: totalPrice,
+        user: { id: user.sub },
+        product: { id: product.id },
+      });
+      await manager.save(Order, newOrder);
+      return {
+        message: 'Transaksi berhasil! Pesanan anda telah dibuat',
+        data: {
+          orderId: newOrder.id,
+          productName: product.name,
+          quantity: newOrder.quantity,
+          totalPrice: newOrder.totalPrice, 
+          remainingStock: product.quantity,
+        },
+      };
     });
-    await this.orderRepository.save(newOrder);
-
-    return {
-      message: 'Transaksi berhasil! Pesanan Anda telah dibuat',
-      data: {
-        orderId: newOrder.id,
-        productName: product.name,
-        quantity: newOrder.quantity,
-        totalPrice: newOrder.totalPrice,
-        remainingStock: product.quantity,
-      },
-    };
   }
   async findMyOrders(user: any) {
     return await this.orderRepository.find({
